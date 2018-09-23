802002220674b9039374aea9dc2a070586481db8
nadia <nadiafa581@gmail.com>
2018-09-23 11:31:31 +0100

    Start structure for cli.rb - prompt messages and user input collection

--- lib/museum_exhibitions/cli.rb
+++ lib/museum_exhibitions/cli.rb
@@ -1,5 +1,26 @@
 class MuseumExhibitions::CLI
   def call
-    puts ">>>>> TESTING lib/cli.rb <<<<<"
+    puts "Please select what you are interested in:"
+    list_options
   end
-end 
+
+  def list_options
+  	puts "This will be a method that lists three options that I will extract w scraping: "
+  	init_selection = gets.strip
+
+  	case init_selection
+  	when "1"
+  	  puts "This will be a method that lists the 'Special Exhibitions' that I will extract w scraping: "
+  	when "2"
+  	  puts "This will be a method that lists the 'Free exhibitions and displays' that I will extract w scraping: "
+  	when "3" 
+  	  puts "This will be a method that lists the 'Today\'s events'  that I will extract w scraping: "
+  	else 
+  	  system("clear")
+  	  puts "That was not a valid option."
+  	  puts "Here are the options again:"
+  	  puts "\n"
+  	  list_options
+  	end
+  end
+end
