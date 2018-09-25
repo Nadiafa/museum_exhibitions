f62fc1f835c8822c7c8841e14c21e7040e05a1e1
nadia <nadiafa581@gmail.com>
2018-09-25 16:06:11 +0100

    Add START_AGAIN_MESSAGE constant

--- lib/museum_exhibitions/constants.rb
+++ lib/museum_exhibitions/constants.rb
@@ -2,11 +2,12 @@ class Constants
   URL = "http://www.britishmuseum.org/whats_on.aspx"
 
   WELCOME_MESSAGE       = "These are the British Museum's main exhibition sections:"
-  LEARN_MORE_MESSAGE    = "\n Select the number of the list you would like to learn more about."
+  LEARN_MORE_MESSAGE    = "\nSelect the number of the list you would like to learn more about."
   SPECIAL_MESSAGE       = "These are the current 'Special Exhibitions':"
   FREE_MESSAGE          = "These are the current 'Free exhibitions and displays':"
   TODAY_MESSAGE         = "These are 'Today\'s events':"
-  INVALID_INPUT_MESSAGE = "\n That was not a valid option. Here are the options again:"
-  INVALID_INPUT_MESSAGE = "\n Goodbye!"
+  INVALID_INPUT_MESSAGE = "\nThat was not a valid option."
+  START_AGAIN_MESSAGE   = "Would you like to start again? (Y/n)"
+  EXIT_MESSAGE          = "\nGoodbye!"
 
 end 
