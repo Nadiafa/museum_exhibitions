2ab3f416c8a9e9fb4ff7bdb8f48ffaa121a40a0e
nadia <nadiafa581@gmail.com>
2018-09-25 16:20:34 +0100

    Refractor with a method for iteration to be used within the file

--- lib/museum_exhibitions/exhibitions_listing.rb
+++ lib/museum_exhibitions/exhibitions_listing.rb
@@ -1,5 +1,4 @@
 class ExhibitionsListing
-  attr_accessor :name
 
   def self.menu1_listing
     selected_titles = Scraper.scrape_main_menu(Constants::URL)
@@ -9,32 +8,26 @@ class ExhibitionsListing
   end
 
   def self.menu2_specials
-  	exhibitions = Scraper.scrape_special_menu(Constants::URL)
-  	exhibitions.each_with_index do |e,i|
-  		puts "\n"
-  		puts "#{i+1}. '#{e[:event_name]}'"
-  		puts "  Follow this link to learn more: '#{e[:event_url]}' "
-  		puts "\n"
-  	end
+  	@exhibitions = Scraper.scrape_special_menu(Constants::URL)
+    menu2_iterations
   end
 
   def self.menu2_free
-  	exhibitions = Scraper.scrape_free_menu(Constants::URL)
-  	exhibitions.each_with_index do |e,i|
-  		puts "\n"
-  		puts "#{i+1}. '#{e[:event_name]}'"
-  		puts "  Follow this link to learn more: '#{e[:event_url]}' "
-  		puts "\n"
-  	end
+  	@exhibitions = Scraper.scrape_free_menu(Constants::URL)
+    menu2_iterations
   end 
 
   def self.menu2_today
-	exhibitions = Scraper.scrape_today_menu(Constants::URL)
-  	exhibitions.each_with_index do |e,i|
-  		puts "\n"
-  		puts "#{i+1}. '#{e[:event_name]}'"
-  		puts "  Follow this link to learn more: '#{e[:event_url]}' "
-  		puts "\n"
-  	end
+	  @exhibitions = Scraper.scrape_today_menu(Constants::URL)
+    menu2_iterations
   end
+
+  def self.menu2_iterations
+    @exhibitions.each_with_index do |e,i|
+      puts "\n"
+      puts "#{i+1}. '#{e[:event_name]}'"
+      puts "  Follow this link to learn more: '#{e[:event_url]}' "
+    end
+  end
+
 end 
