70ea1c52e25d513d96b5de543fd56bd5e0f9db91
nadia <nadiafa581@gmail.com>
2018-09-25 12:23:04 +0100

    Add constants for messages to be used by cli.rb

--- lib/museum_exhibitions/constants.rb
+++ lib/museum_exhibitions/constants.rb
@@ -1,3 +1,11 @@
 class Constants
   URL = "http://www.britishmuseum.org/whats_on.aspx"
+
+  WELCOME_MESSAGE       = "These are the British Museum's main exhibition sections:"
+  LEARN_MORE_MESSAGE    = "\n Select the number of the list you would like to learn more about."
+  SPECIAL_MESSAGE       = "These are the current 'Special Exhibitions':"
+  FREE_MESSAGE          = "These are the current 'Free exhibitions and displays':"
+  TODAY_MESSAGE         = "These are 'Today\'s events':"
+  INVALID_INPUT_MESSAGE = "\n That was not a valid option. Here are the options again:"
+
 end 
