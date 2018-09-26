88f31ad59d06d47f2d63784b8cdd5a2ef6177991
nadia <nadiafa581@gmail.com>
2018-09-26 14:26:31 +0100

    Change the calls in menu2 to call the new Scraper refractored method with the new selector's contants as arguments

--- lib/museum_exhibitions/exhibitions_listing.rb
+++ lib/museum_exhibitions/exhibitions_listing.rb
@@ -9,19 +9,16 @@ class ExhibitionsListing
 
   def self.menu2_specials
     @exhibitions  = Scraper.scrape_secondary_menu(Constants::URL, Constants::SPECIFIC_SELECTOR_SPECIAL, Constants::NAME_SELECTOR_SPECIAL)
-  	# @exhibitions = Scraper.scrape_special_menu(Constants::URL)
     menu2_iterations
   end
 
   def self.menu2_free
     @exhibitions  = Scraper.scrape_secondary_menu(Constants::URL, Constants::SPECIFIC_SELECTOR_FREE, Constants::NAME_SELECTOR_FREE)
-  	# @exhibitions = Scraper.scrape_free_menu(Constants::URL)
     menu2_iterations
   end 
 
   def self.menu2_today
     @exhibitions  = Scraper.scrape_secondary_menu(Constants::URL, Constants::SPECIFIC_SELECTOR_TODAY, Constants::NAME_SELECTOR_TODAY)
-	  # @exhibitions = Scraper.scrape_today_menu(Constants::URL)
     menu2_iterations
   end
 
@@ -32,5 +29,5 @@ class ExhibitionsListing
       puts "  Follow this link to learn more: '#{e[:event_url]}'"
     end
   end
-  
+
 end 
