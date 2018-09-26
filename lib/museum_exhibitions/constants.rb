f331e8b635fa67b31b4f4a251b7e4e6057429228
nadia <nadiafa581@gmail.com>
2018-09-26 14:54:29 +0100

    Add message constant with message to select again

--- lib/museum_exhibitions/constants.rb
+++ lib/museum_exhibitions/constants.rb
@@ -1,26 +1,23 @@
 class Constants
+  #URL to scrape
   URL = "http://www.britishmuseum.org/whats_on.aspx"
 
+  #Messages
   WELCOME_MESSAGE       = "These are the British Museum's main exhibition sections:"
   LEARN_MORE_MESSAGE    = "\nSelect the number of the list you would like to learn more about."
   SPECIAL_MESSAGE       = "These are the current 'Special Exhibitions':"
   FREE_MESSAGE          = "These are the current 'Free exhibitions and displays':"
   TODAY_MESSAGE         = "These are 'Today\'s events':"
   INVALID_INPUT_MESSAGE = "\nThat was not a valid option."
+  SELECT_AGAIN_MESSAGE  = "Please choose from the following:"
   START_AGAIN_MESSAGE   = "Would you like to start again? (Y/n)"
   EXIT_MESSAGE          = "\nGoodbye!"
 
+  #Selectors
   SPECIFIC_SELECTOR_SPECIAL = ".grid_12 .grid_12 .grid_4"
   SPECIFIC_SELECTOR_FREE    = ".container .grid_12 .grid_4 .titleSpacer"
   SPECIFIC_SELECTOR_TODAY   = ".container .grid_12 ul li"
   NAME_SELECTOR_SPECIAL     = "h3 a"
   NAME_SELECTOR_FREE        = "h3"
   NAME_SELECTOR_TODAY       = "a .grid_6"
-  
- 
 end 
-
-# class Selectors 
-#   SPECIAL_SELECTOR_MENU2 = ".container .grid_12 ul li"
-#   SPECIAL_SELECTOR_MENU2_NAME =  "a .grid_6")
-# end
