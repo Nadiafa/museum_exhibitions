5c8fea6ca96a0dafac07ee55f707cd80cf8514c6
nadia <nadiafa581@gmail.com>
2018-10-02 13:08:43 +0100

    Edit message constants for category

--- lib/museum_exhibitions/constants.rb
+++ lib/museum_exhibitions/constants.rb
@@ -5,9 +5,9 @@ class Constants
   #Messages
   WELCOME_MESSAGE       = "These are the British Museum's main exhibition sections:"
   LEARN_MORE_MESSAGE    = "\nSelect the number of the list you would like to learn more about."
-  SPECIAL_MESSAGE       = "These are the current 'Special Exhibitions':"
-  FREE_MESSAGE          = "These are the current 'Free exhibitions and displays':"
-  TODAY_MESSAGE         = "These are 'Today\'s events':"
+  MESSAGE_CAT1       = "These are the current 'Special Exhibitions':"
+  MESSAGE_CAT2          = "These are the current 'Free exhibitions and displays':"
+  MESSAGE_CAT3         = "These are 'Today\'s events':"
   INVALID_INPUT_MESSAGE = "\nThat was not a valid option."
   SELECT_AGAIN_MESSAGE  = "Please choose from the following:"
   START_AGAIN_MESSAGE   = "Would you like to start again? (Y/n)"
