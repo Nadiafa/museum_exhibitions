e4835b81a2c218044453b1978de47815d21a85ed
nadia <nadiafa581@gmail.com>
2018-10-02 13:10:16 +0100

    Edit to have file call the two scraper mmethod in Scraper class merging secondary menu calls into one

--- lib/museum_exhibitions/exhibitions_listing.rb
+++ lib/museum_exhibitions/exhibitions_listing.rb
@@ -1,33 +1,25 @@
 class ExhibitionsListing
 
-  def self.menu1_listing
-    selected_titles = Scraper.scrape_main_menu(Constants::URL)
-    selected_titles.each_with_index do |e, i|
-      puts "  #{i+1}. #{e}."
-    end     
+  def self.first_level
+    Scraper.scrape_main_menu(Constants::URL)
   end
 
-  def self.menu2_specials
-    @exhibitions  = Scraper.scrape_secondary_menu(Constants::URL, Constants::SPECIFIC_SELECTOR_SPECIAL, Constants::NAME_SELECTOR_SPECIAL)
-    menu2_iterations
+  def self.second_level
+    Event.create_from_collection(Scraper.scrape_secondary_menu(Constants::URL, Constants::SPECIFIC_SELECTOR_SPECIAL, Constants::NAME_SELECTOR_SPECIAL,"cat1"))
+    Event.create_from_collection(Scraper.scrape_secondary_menu(Constants::URL, Constants::SPECIFIC_SELECTOR_FREE, Constants::NAME_SELECTOR_FREE,"cat2"))
+    Event.create_from_collection(Scraper.scrape_secondary_menu(Constants::URL, Constants::SPECIFIC_SELECTOR_TODAY, Constants::NAME_SELECTOR_TODAY,"cat3"))
+    Event.all
   end
 
-  def self.menu2_free
-    @exhibitions  = Scraper.scrape_secondary_menu(Constants::URL, Constants::SPECIFIC_SELECTOR_FREE, Constants::NAME_SELECTOR_FREE)
-    menu2_iterations
-  end 
 
-  def self.menu2_today
-    @exhibitions  = Scraper.scrape_secondary_menu(Constants::URL, Constants::SPECIFIC_SELECTOR_TODAY, Constants::NAME_SELECTOR_TODAY)
-    menu2_iterations
-  end
 
-  def self.menu2_iterations
-    @exhibitions.each_with_index do |e,i|
-      puts "\n"
-      puts "#{i+1}. '#{e[:event_name]}'"
-      puts "  Follow this link to learn more: '#{e[:event_url]}'"
-    end
-  end
+
+  # def self.menu2_iterations
+  #   @exhibitions.each_with_index do |e,i|
+  #     puts "\n"
+  #     puts "#{i+1}. '#{e[:event_name]}'"
+  #     puts "  Follow this link to learn more: '#{e[:event_url]}'"
+  #   end
+  # end
 
 end 
